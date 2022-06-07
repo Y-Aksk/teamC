df40ad5b40a9607c8760d1949766c1ca82159a19
kbys_tkhr <kobayashi.takahiro@solekia.co.jp>
2022-06-07 16:33:09 +0900

    6/7 16:33更新

--- src/main/java/solekia/demo/DemoApplication.java
+++ src/main/java/solekia/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
 @ServletComponentScan
-@MapperScan("solekia.demo.repository")
+@MapperScan("solekia.demo.fastfood.repository")
 public class DemoApplication {
 
 	public static void main(String[] args) {
