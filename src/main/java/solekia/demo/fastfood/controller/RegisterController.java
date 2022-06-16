8027f7df3627a4e74d7ecb3890aa58905c004122
kbys_tkhr <kobayashi.takahiro@solekia.co.jp>
2022-06-16 11:03:28 +0900

    6/16 11:03 更新

---
+++ src/main/java/solekia/demo/fastfood/controller/RegisterController.java
@@ -0,0 +1,265 @@
+package solekia.demo.fastfood.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import solekia.demo.fastfood.model.*;
+import solekia.demo.fastfood.repository.*;
+
+//RegisterController
+@Controller
+
+//処理対象とするURL（今回はfastfood）を指定
+@RequestMapping("fastfood")
+public class RegisterController{
+
+    //テーブルのアクセス用クラスを追加
+    @Autowired
+    //RagesterMapperをインスタンス化
+    RegisterMapper registerMapper;
+
+    @Autowired
+    LoginMapper LoginMapper;
+
+/*----　↓↓小林さんと合わせるときにちゃんと動くかチェック↓↓　----*/
+
+/*----　ホーム画面　(http://localhost:8080/fastfood/home)　----*/
+    //人気メニュートップ3の表示
+    @GetMapping("home")
+    public String home(Model model){
+        //RegisterPageModelクラスをpageとして扱う
+        RegisterPageModel page = new RegisterPageModel();
+        //pageのtitleメソッドに処理を追加
+        page.title = "ホーム(java)";
+
+        //page.login=1;
+
+        //pageのlistにRegisterMapperクラスのfindRank()メソッドの値を格納
+        page.list = registerMapper.findRank();
+        //モデルにページインスタンスを設定
+        model.addAttribute("page",page);
+        //テンプレートファイルを指定
+        return "fastfood/home";
+    }
+
+
+/*----　お問い合わせ　(http://localhost:8080/fastfood/qa)　完了----*/
+    //お問い合わせ画面
+    @GetMapping("qa")
+    public String qa(Model model){
+        //ページインスタンスを作って、タイトルを設定
+        RegisterPageModel page = new RegisterPageModel();
+        page.title = "お問い合わせ(java)";
+        //モデルにページインスタンスを設定
+        model.addAttribute("page", page);
+        //テンプレートファイルを指定
+        return "fastfood/qa";
+    }
+
+    //お問い合わせ処理
+    @PostMapping("insertqa")
+    public String insertQa(@ModelAttribute RegisterPageModel page, Model model){
+
+        //タイトルを設定
+        page.title = "お問い合わせ(java)";
+        //画面で入力した更新データをパラメータに設定
+        registerMapper.insertQa(page.qa_first_name, page.qa_last_name, page.question, page.qa_mail);
+        //モデルにページインスタンスを設定
+        model.addAttribute("page", page);
+        //テンプレートファイルを指定
+        return "fastfood/qa";
+    }
+
+/*----　新規会員登録　(http://localhost:8080/fastfood/register)　完了----*/
+    //新規会員登録　画面
+    @GetMapping("{id}/register")//先頭に{id}追加、ログアウト対策
+    public String register(Model model){
+        //ページインスタンスを作って、タイトルを設定
+        RegisterPageModel page = new RegisterPageModel();
+        page.title = "新規会員登録(java)";
+        //pageのlistにfinShop()メソッドの値を格納
+        page.list = registerMapper.findShop(); 
+        //モデルにページインスタンスを設定
+        model.addAttribute("page", page);
+        //テンプレートファイルを指定
+        return "fastfood/register";
+    }
+
+    
+    //新規会員登録　処理　URL小林さんに聞く。
+    @PostMapping("{id}/register")
+    public String insertCust(
+        //@ModelAttribute RegisterPageModel page, Model model){
+        @RequestParam("password") String password, 
+        @RequestParam("first_name") String first_name, 
+        @RequestParam("last_name") String last_name,
+        @RequestParam("tell_no") String tell_no,
+        @RequestParam("mail") String mail, 
+        @RequestParam("shop_name") String shop_name,
+        Model model
+    ){
+        //LoginPageModel型オブジェクトのpageを生成
+        LoginPageModel page = new LoginPageModel();
+        page.message = "新規登録されました。";
+        //画面で入力した更新データをパラメータに設定
+        registerMapper.insertCust(password, first_name, last_name, tell_no, mail, shop_name);
+        //モデルにページインスタンスを設定
+        model.addAttribute("page", page);
+        //テンプレートファイルを指定
+        return "fastfood/login";
+    }
+    
+    @GetMapping("register")//loginから飛んだ場合、{id}は先頭に不要
+    public String register2(Model model){
+        //ページインスタンスを作って、タイトルを設定
+        RegisterPageModel page = new RegisterPageModel();
+        page.title = "新規会員登録(java)";
+        //pageのlistにfinShop()メソッドの値を格納
+        page.list = registerMapper.findShop(); 
+        //モデルにページインスタンスを設定
+        model.addAttribute("page", page);
+        //テンプレートファイルを指定
+        return "fastfood/register";
+    }
+
+    
+    //新規会員登録　処理　URL小林さんに聞く。
+    //loginから飛んだ場合、{id}は先頭に不要
+    @PostMapping("register")
+    public String insertCust2(
+        //@ModelAttribute RegisterPageModel page, Model model){
+        @RequestParam("password") String password, 
+        @RequestParam("first_name") String first_name, 
+        @RequestParam("last_name") String last_name,
+        @RequestParam("tell_no") String tell_no,
+        @RequestParam("mail") String mail, 
+        @RequestParam("shop_name") String shop_name,
+        Model model
+    ){
+        //追加
+        LoginPageModel page = new LoginPageModel();
+        //画面で入力した更新データをパラメータに設定
+        registerMapper.insertCust(password, first_name, last_name, tell_no, mail, shop_name);
+        //モデルにページインスタンスを設定
+        model.addAttribute("page", page);
+        //テンプレートファイルを指定
+        return "fastfood/login";
+    }
+
+
+/*----　新規従業員登録　(http://localhost:8080/fastfood/register_emp)　完了----*/
+    //新規従業員登録画面
+    @GetMapping("register_emp")
+    public String registerEmp(Model model){
+        //ページインスタンスを作って、タイトルを設定
+        RegisterPageModel page = new RegisterPageModel();
+        page.title = "新規従業員登録(java)";
+        //pageのlistにfinShop()メソッドの値を格納
+        page.list = registerMapper.findShop(); 
+        //モデルにページインスタンスを設定
+        model.addAttribute("page", page);
+        //テンプレートファイルを指定
+        return "fastfood/register_emp";
+    }
+
+    /*
+    //新規従業員登録処理　URL小林さんに聞く
+    @PostMapping("login_emp")
+    public String insertEmp(@ModelAttribute RegisterPageModel page, Model model){
+        //画面で入力した更新データをパラメータに設定
+        registerMapper.insertEmp(page.password, page.first_name, page.last_name, page.tell_no, page.mail, page.shop_name, page.login);
+        //モデルにページインスタンスを設定
+        model.addAttribute("page", page);
+        //テンプレートファイルを指定
+        return "fastfood/login_emp";
+    }
+ */
+
+/*----　↓↓小林さんと合わせるときにちゃんと動くかチェック↓↓　----*/
+
+/*----　顧客　登録情報変更　(http://localhost:8080/fastfood/edit)　----*/
+    //顧客登録情報変更画面　URL変更する必要ありそう loginとidの値はセッション保持してたらいらない…？？
+    @GetMapping("edit/{id}")
+    public String edit(@PathVariable("id") int id, @PathVariable("login") int login, Model model){
+        //ページインスタンスを作ってタイトルを設定
+        RegisterPageModel page = new RegisterPageModel();
+        page.title = "登録情報変更(java)";
+        //IDをキーにデータを検索
+        var register = registerMapper.findById(id);
+        //urlのloginにページのloginの値を入れる？違う気がする。
+        login = page.login;
+        //取得データをページに設定
+        page.customer_id = register.customer_id;
+        page.password = register.password;
+        page.first_name = register.first_name;
+        page.last_name = register.last_name;
+        page.tell_no = register.tell_no;
+        page.mail = register.mail;
+        page.shop_name = register.shop_name;
+        page.login = register.login;
+        //モデルにページインスタンスを設定
+        model.addAttribute("page", page);
+        //テンプレートファイルを指定
+        return "fastfood/edit";
+    }
+
+    /* 
+    //顧客登録情報変更処理　URL小林さんに聞く
+    @PostMapping("mypage/{id}/{login}")
+    public String edit(@ModelAttribute RegisterPageModel page, Model model){
+        //画面で入力した更新データをパラメータに設定
+        registerMapper.update(page.customer_id, page.password, page.first_name, page.last_name, page.tell_no, page.mail, page.shop_name, page.login);
+        //更新後のデータを取得
+        page.list = registerMapper.findRe();
+        //モデルにページインスタンスを設定
+        model.addAttribute("page", page);
+        //テンプレートファイルを指定
+        return "fastfood/mypage";
+    }
+*/
+
+/*----　従業員　登録情報変更　(http://localhost:8080/fastfood/edit_emp)　----*/
+    //従業員　登録情報変更　画面　URL変更する必要ありそう　loginとidの値はセッション保持してたらいらない…？？
+    @GetMapping("edit_emp/{id}/{login}")
+    public String editEmp(@PathVariable("id") int id, @PathVariable("login") int login, Model model){
+        //ページインスタンスを作ってタイトルを設定
+        RegisterPageModel page = new RegisterPageModel();
+        page.title = "登録情報変更(java)";
+        //IDをキーにデータを検索
+        var register = registerMapper.findById(id);
+        //urlのloginにページのloginの値を入れる？違う気がする。
+        login = page.login;
+        //取得データをページに設定
+        page.customer_id = register.customer_id;
+        page.password = register.password;
+        page.first_name = register.first_name;
+        page.last_name = register.last_name;
+        page.tell_no = register.tell_no;
+        page.mail = register.mail;
+        page.shop_name = register.shop_name;
+        page.login = register.login;
+        //モデルにページインスタンスを設定
+        model.addAttribute("page", page);
+        //テンプレートファイルを指定
+        return "fastfood/edit_emp";
+    }
+
+    /* 
+    //従業員　登録情報変更　処理　URL小林さんに聞く
+    @PostMapping("mypage_emp/{id}/{login}")
+    public String editEmp(@ModelAttribute RegisterPageModel page, Model model){
+        //画面で入力した更新データをパラメータに設定
+        registerMapper.update(page.customer_id, page.password, page.first_name, page.last_name, page.tell_no, page.mail, page.shop_name, page.login);
+        //更新後のデータを取得
+        page.list = registerMapper.findRe();
+        //モデルにページインスタンスを設定
+        model.addAttribute("page", page);
+        //テンプレートファイルを指定
+        return "fastfood/mypage_emp";
+    }
+    */
+}
+
+
---
+++ src/main/java/solekia/demo/fastfood/repository/RegisterModel.java
@@ -0,0 +1,27 @@
+//package solekia.demo.fastfood.controller.repository;
+package solekia.demo.fastfood.repository;
+
+public class RegisterModel {
+    //お問い合わせ
+    public int qa_id;
+    public String qa_first_name;
+    public String qa_last_name;
+    public String question;
+    public String qa_mail;
+
+    //会員登録関連
+    public int customer_id;
+    public String password;
+    public String first_name;
+    public String last_name;
+    public String tell_no;
+    public String mail;
+    public int login;
+    public int authority;
+    public String shop_name;
+
+    //ホーム画面
+    public String product_pic;
+    public String product_name;
+    public String rank;
+}
